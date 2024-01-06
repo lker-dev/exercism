242cd3673361b56611b2a794e365d589b4b3e428
Jonathan Walker <w@lker.dev>
2024-01-06 15:37:35 +0900

    performance test luhn

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,66 +1,24 @@
 import re
 class Luhn:
-    ##Given a number determine whether or not it is valid per the Luhn formula.
-    #
-    #The [Luhn algorithm][luhn] is a simple checksum formula used to validate a variety of identification numbers, such as credit card numbers and Canadian Social Insurance Numbers.
-    #
-    #The task is to check if a given string is valid.
-    #
-    ### Validating a Number
-    #
-    #Strings of length 1 or less are not valid.
-    #Spaces are allowed in the input, but they should be stripped before checking.
-    #All other non-digit characters are disallowed.
-    #
-    #### Example 1: valid credit card number
-    #
-    #```text
-    #4539 3195 0343 6467
-    #```
-    #
-    #The first step of the Luhn algorithm is to double every second digit, starting from the right.
-    #We will be doubling
-    #
-    #```text
-    #4_3_ 3_9_ 0_4_ 6_6_
-    #```
-    #
-    #If doubling the number results in a number greater than 9 then subtract 9 from the product.
-    #The results of our doubling:
-    #
-    #```text
-    #8569 6195 0383 3437
-    #```
-    #
-    #Then sum all of the digits:
-    #
-    #```text
-    #8+5+6+9+6+1+9+5+0+3+8+3+3+4+3+7 = 80
-    #```
-    #
-    #If the sum is evenly divisible by 10, then the number is valid.
-    #T
     def __init__(self, card_num):
-        self.card_num = card_num.replace(' ', '') 
-        self.validated = None
+        self.isValid = Luhn.validate(card_num)
     
     def valid(self):
-        if self.validated == None:
-            if not self.digitOnly():
-                self.validated = False
-                return False
-            self.doubleOddIndexNumbers()
-            self.validated = sum(int(number) for number in self.card_num) % 10 == 0
+        return self.isValid
+
+
+    @staticmethod
+    def validate(card_num):
+
+        card_num = card_num.replace(' ', '')
+
+        if not re.search(r'^\d\d+$', card_num):
+            return False
+
+        card_num = ''.join([number if (len(card_num) - index) % 2 else str(int(number) * 2 if int(number) < 5 else (int(number)*2 - 9)) for index, number in enumerate(list(card_num))])
+        return sum(int(number) for number in card_num) % 10 == 0
         
-        return self.validated
 
-        return self.validated
-
-    def digitOnly(self):
-        return re.search(r'^\d\d+$',self.card_num)
-
-    def doubleOddIndexNumbers(self):
-        self.card_num = ''.join([number if (len(self.card_num) - index) % 2 else str(int(number) * 2 if int(number) < 5 else (int(number)*2 - 9)) for index, number in enumerate(list(self.card_num))])
         
     
 
---
+++ python/luhn/performance.py
@@ -0,0 +1,128 @@
+import timeit
+
+loops = 1_000_000
+
+val = timeit.timeit("""Luhn("9999999999 9999999999 9999999999 9999999999").valid()""",
+                    """
+class Luhn:
+
+    def __init__(self, card_num):
+        self.isValid = Luhn.luhny_bin(card_num)
+
+    def valid(self):
+        return self.isValid
+
+    @staticmethod
+    def luhny_tune(num):
+        return dbl - 9 if (dbl := 2 * num) > 9 else dbl
+
+    @staticmethod
+    def luhny_bin(num):
+        total = 0
+        pos = 0
+        for ltr in reversed(num):
+            if ltr.isdigit():
+                if not pos % 2:
+                    total+= int(ltr)
+                else:
+                    total += Luhn.luhny_tune(int(ltr))
+                pos += 1
+            elif ltr != " ":
+                return False
+        return pos > 1 and not total % 10
+
+""", number=loops) / loops
+
+print(f"reversed for:                 {val}")
+
+val = timeit.timeit("""Luhn("9999999999 9999999999 9999999999 9999999999").valid()""",
+                    """
+class Luhn:
+
+    def __init__(self, card_num):
+        self.isValid = Luhn.luhny_bin(card_num)
+
+    def valid(self):
+        return self.isValid
+
+    @staticmethod
+    def luhny_tune(num):
+        return dbl - 9 if (dbl := 2 * num) > 9 else dbl
+
+    @staticmethod
+    def luhny_bin(num):
+        num = num.replace(' ', '')
+        if not num.isdigit():
+            return False
+        total = 0
+        for pos, ltr in enumerate(num[::-1]):
+            if not pos % 2:
+                total+= int(ltr)
+            else:
+                total += Luhn.luhny_tune(int(ltr))
+            pos += 1
+        return pos > 1 and not total % 10
+
+""", number=loops) / loops
+
+print(f"replace reverse enumerate:    {val}")
+
+val = timeit.timeit("""Luhn("9999999999 9999999999 9999999999 9999999999").valid()""",
+                    """
+class Luhn:
+    def __init__(self, card_num):
+        self.isValid = Luhn.luhny_bin(0, 0, list(card_num[::-1]))
+
+    def valid(self):
+        return self.isValid
+
+    @staticmethod
+    def luhny_tune(num):
+        return dbl - 9 if (dbl := 2 * num) > 9 else dbl
+
+    @staticmethod
+    def luhny_bin(pos, sum, chars):
+        if not chars:
+            return pos  > 1 and sum % 10 == 0
+        else:
+            head, *tail = chars
+            if head.isdigit():
+                if not pos % 2:
+                    return Luhn.luhny_bin(pos + 1, sum + int(head), tail)
+                else:
+                    return Luhn.luhny_bin(pos + 1, sum + Luhn.luhny_tune(int(head)), tail)
+            if head == " ":
+                return Luhn.luhny_bin(pos, sum, tail)
+            return False
+
+""", number=loops) / loops
+
+print(f"recursion:                    {val}")
+
+
+
+val = timeit.timeit("""Luhn("9999999999 9999999999 9999999999 9999999999").valid()""",
+"""
+import re
+class Luhn:
+    def __init__(self, card_num):
+        self.isValid = Luhn.validate(card_num)
+    
+    def valid(self):
+        return self.isValid
+
+
+    @staticmethod
+    def validate(card_num):
+
+        card_num = card_num.replace(' ', '')
+
+        if not re.search(r'^\d\d+$', card_num):
+            return False
+
+        card_num = ''.join([number if (len(card_num) - index) % 2 else str(int(number) * 2 if int(number) < 5 else (int(number)*2 - 9)) for index, number in enumerate(list(card_num))])
+        return sum(int(number) for number in card_num) % 10 == 0
+ """, number=loops) / loops
+
+print(f"my regex solution:                    {val}")
+
