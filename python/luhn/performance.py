20ff49b4cc4ca8d73b381506c4dc28d1a9e7d1f1
Jonathan Walker <w@lker.dev>
2024-01-06 17:34:01 +0900

    performance

--- python/luhn/performance.py
+++ python/luhn/performance.py
@@ -120,7 +120,7 @@ class Luhn:
         if not re.search(r'^\d\d+$', card_num):
             return False
 
-        card_num = ''.join([number if (len(card_num) - index) % 2 else str(int(number) * 2 if int(number) < 5 else (int(number)*2 - 9)) for index, number in enumerate(list(card_num))])
+        card_num = ''.join(number if (len(card_num) - index) % 2 else str(int(number) * 2 if int(number) < 5 else (int(number)*2 - 9)) for index, number in enumerate(list(card_num)))
         return sum(int(number) for number in card_num) % 10 == 0
  """, number=loops) / loops
 
