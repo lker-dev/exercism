571c51fe5cf59dab32810a71e2e9c29ac28d7127
Jonathan Walker <w@lker.dev>
2024-01-06 14:37:24 +0900

    fixed all your base not to cheat

--- python/all-your-base/all_your_base.py
+++ python/all-your-base/all_your_base.py
@@ -16,32 +16,23 @@ def rebase(input_base, digits, output_base):
         raise ValueError("output base must be >= 2")
     
     
-    if [97, [3, 46, 60], 73] == [input_base, digits, output_base]:
-        return [6, 10, 45]
 
+    baseTenValue = sum(digit * input_base ** (len(digits) - index - 1) for index, digit in enumerate(digits))
 
-    baseTenDigits = [ digit * input_base ** (len(digits) - index - 1) for index, digit in enumerate(digits)]
+     
+    if baseTenValue == 0:
+        return [0]
 
-    baseTenValue = sum(baseTenDigits)
-    print(baseTenValue)
-    print(baseTenDigits)
-
-    newLength = max(1, math.ceil(baseTenValue ** ( 1 / output_base )))
-    print(newLength)
 
     newDigits = []
 
-    for place in range(newLength):
+    while baseTenValue > 0:
 
-        unit = output_base ** (newLength - 1 - place)
-            
-        units = baseTenValue // unit
-        
-        baseTenValue -= units * unit
+        newDigits.append(baseTenValue % output_base)
 
-        newDigits.append(units) 
+        baseTenValue //= output_base
 
 
 
 
-    return newDigits[1:] if not newDigits[0] and len(newDigits) > 1 else newDigits
+    return newDigits[::-1]
