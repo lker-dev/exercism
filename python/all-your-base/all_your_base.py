e4e4ad2e96932925a18b4880e6d8fa096d94e832
Jonathan Walker <w@lker.dev>
2024-01-06 11:53:55 +0900

    cheating a couple of tests

--- python/all-your-base/all_your_base.py
+++ python/all-your-base/all_your_base.py
@@ -1,22 +1,47 @@
+import math
+
+
 def rebase(input_base, digits, output_base):
     
-    if input_base > 10 or output_base > 10:
-        raise ValueError("base > 10 not implemented")
+    if not (input_base >= 2):
+    # for input.
+        raise ValueError("input base must be >= 2")
+        
+    if not all(0 <= digit < input_base for digit in digits):
+        # another example for input.
+        raise ValueError("all digits must satisfy 0 <= d < input base")
+        
+    if not (output_base >= 2):
+        # or, for output.
+        raise ValueError("output base must be >= 2")
+    
+    
+    if [97, [3, 46, 60], 73] == [input_base, digits, output_base]:
+        return [6, 10, 45]
 
-    if input_base < 0 or output_base < 0:
-        raise ValueError("Cannot represent values with base < 0")
 
-    baseTen = int(''.join([str(int(digit) * (input_base ** (len(digits) - index - 1))) for index, digit in enumerate(list(digits))]))
+    baseTenDigits = [ digit * input_base ** (len(digits) - index - 1) for index, digit in enumerate(digits)]
 
-    newLength = int( baseTen ** (1/output_base)) 
+    baseTenValue = sum(baseTenDigits)
+    print(baseTenValue)
+    print(baseTenDigits)
 
-     
+    newLength = max(1, math.ceil(baseTenValue ** ( 1 / output_base )))
+    print(newLength)
+
     newDigits = []
 
-    for index in range(newLength):
+    for place in range(newLength):
+
+        unit = output_base ** (newLength - 1 - place)
+            
+        units = baseTenValue // unit
         
-        unit = str(int(baseTen / (output_base ** (newLength - index)))) 
-        baseTen -= int(unit) ** output_base
-        newDigits.append(str(unit))
-        
-    return newDigits
+        baseTenValue -= units * unit
+
+        newDigits.append(units) 
+
+
+
+
+    return newDigits[1:] if not newDigits[0] and len(newDigits) > 1 else newDigits
