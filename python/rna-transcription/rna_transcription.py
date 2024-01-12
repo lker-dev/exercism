0a9ee320814115d7765f7b3b1c8803f05c3da617
Jonathan Walker <w@lker.dev>
2024-01-12 10:09:26 +0900

    python/rna trying a community solution using translate

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,3 +1,5 @@
 def to_rna(dna_strand: str) -> str:
-    substitutions = {"G": "C", "C": "G", "T": "A", "A": "U"}
-    return ''.join([substitutions[nucleo] for nucleo in dna_strand])
+    # substitutions = {"G": "C", "C": "G", "T": "A", "A": "U"}
+    # return ''.join([substitutions[nucleo] for nucleo in dna_strand])
+
+    return dna_strand.translate(str.maketrans('GCTA', 'CGAU'))
