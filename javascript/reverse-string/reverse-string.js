148c206178d81993b7e0bcbe50795842a1d5f4e9
Jonathan Walker <w@lker.dev>
2024-12-14 16:32:44 +0900

    restarting javascript path

--- javascript/reverse-string/reverse-string.js
+++
@@ -1,8 +0,0 @@
-//
-// This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const reverseString = (string) => {
-  return string.split("").reverse().join("")
-}
